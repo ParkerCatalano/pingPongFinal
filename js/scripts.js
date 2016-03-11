84aa0e0de2e8f551b4cc822490d6618872041083
ParkerCatalano <parkercatalano@gmail.com>
2016-03-10 20:17:28 -0800

    create append function for numLoop div to display numbers 1 through 100 with ping/pong/pingpong in place of 3 5 and 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function() {
-  $('#blanks form').submit(function() {
+  $('#userInput form').submit(function() {
     for (i = 1; i <= 100; i++) {
-      if ( i % 15 === 0) {
-        console.log("pingpong");
+    if ( i % 15 === 0) {
+        $('#numLoop').append('<span>pingpong</span>');
       }
-      else if( i % 3 === 0) {
-        console.log("ping");
+    else if( i % 3 === 0) {
+        $('#numLoop').append('<span>ping</span>');
       }
-      else if ( i % 5 === 0) {
-        console.log("pong");
+    else if ( i % 5 === 0) {
+        $('#numLoop').append('<span>pong</span>');
       }
-      else {
-        console.log(i)
+    else {
+        $('#numLoop').append('<span>'+i+'</span>');
         event.preventDefault();
       }
-  }
+    }
 });
 });
