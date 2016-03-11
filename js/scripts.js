027f61c06852230298f8c63c840c2940cbdafb7a
ParkerCatalano <parkercatalano@gmail.com>
2016-03-10 19:25:03 -0800

    sets up for loop

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+for (i = 1; i <= 100; i++) {
+  if ( i % 15 === 0) {
+  console.log("pingpong");
+  }
+   else if( i % 3 === 0) {
+  console.log("ping");
+  }
+  else if ( i % 5 === 0) {
+  console.log("pong");
+}
+  console.log(i)
+}
