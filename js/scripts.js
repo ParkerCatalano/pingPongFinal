6dc93c4c5703aeaa3befc7fce55dd8471dbf38a4
ParkerCatalano <parkercatalano@gmail.com>
2016-03-10 19:51:32 -0800

    prevent default

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 
-for (i = 1; i <= 100; i++) {
-  if ( i % 15 === 0) {
-  console.log("pingpong");
+$(document).ready(function() {
+  $('#blanks form').submit(function() {
+    for (i = 1; i <= 100; i++) {
+      if ( i % 15 === 0) {
+        console.log("pingpong");
+      }
+      else if( i % 3 === 0) {
+        console.log("ping");
+      }
+      else if ( i % 5 === 0) {
+        console.log("pong");
+      }
+      else {
+        console.log(i)
+        event.preventDefault();
+      }
   }
-   else if( i % 3 === 0) {
-  console.log("ping");
-  }
-  else if ( i % 5 === 0) {
-  console.log("pong");
-}
-  console.log(i)
-}
+});
+});
